6823f88bad3e8ceb3778d67bfce9b82652da3d09
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-09-30 00:25:08 -0300

    Refatoramento

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,5 +1,4 @@
-
-$(function() {
+( () => {
 
 	/*
 		Cliente solicita lista de questoes
@@ -9,86 +8,119 @@ $(function() {
 		Servidor recebe e retorna um array de resultado (true or false)
 	*/
 
-	var listOfQuestions = [];
-	var answer = [];
+	var listOfQuestions = []
+	var answer = []
 
 	/* Connecting to web server */
-	var socket = io('http://localhost:3000');
+	var socket = io('http://localhost:3000')
 
 	// Request questions
 	socket.on('connect', function(){
-    console.log("conectado!");
-		socket.emit('getQuestions');
-	});
+    console.log("conectado!")
+		socket.emit('getQuestions')
+	})
 
 	// Get response of the server
 	socket.on('questions', function(list){
 
-			/*
-				"list" is a json with questions (title and alternatives) returned from the server
-			*/
+		/*
+			"list" is a json with questions (title and alternatives) returned from the server
+		*/
+		listOfQuestions = list
 
-			listOfQuestions = list;
+		/* Avalia e muda de question */
+		// Show the first question
+		modelHTML(listOfQuestions[0])
 
-			/* Avalia e muda de question */
+		var currentQuestion = 0
+		var uploadProgressBar = 0
+		var progress_bar = document.querySelector(".progress-bar")
+		var confirm = document.getElementById('confirm')
 
-			// Show the first question
-			modelHTML(listOfQuestions[0]);
+		confirm.addEventListener('click', () => {
+			//Executa Loop entre todas as Radio buttons com o name de value
+			var inputs = document.getElementsByTagName('input')
+			
+			// Capturar a resposta marcada
+			addActionInListOfElements(inputs, element => {
+				if (element.checked) {
+					answer.push(element.value) //captura resposta
+					confirm.classList.add('disabled') //desabilita o botao para prox question
+					currentQuestion++
+				}
+			})
 
-			var currentQuestion = 0;
-			var uploadProgressBar = 0;
-			var progress_bar = document.querySelector(".progress-bar");
-
-			$('#confirm').click(function() {
-
-			    //Executa Loop entre todas as Radio buttons com o name de value
-			    $('input:radio[name=value]').each(function() {
-			        //Verifica qual está selecionado
-			        if ($(this).is(':checked')) {
-			            answer.push($(this).val());	//captura resposta
-			            $('#confirm').addClass('disabled'); //desabilita o botao para prox question
-			            currentQuestion++;
-			        }
-			    });
-
-					// Show the next question
-			    if (currentQuestion < listOfQuestions.length){
-						modelHTML(listOfQuestions[currentQuestion]);
-					}
-			    else { //Finalizou Quiz
-						console.log(answer);
-						localStorage.setItem("answer", answer);
-						location.href='result.html';
-			    }
-
-			    uploadProgressBar += (100 / listOfQuestions.length);
-			    progress_bar.style.width = uploadProgressBar+"%";
-			});
-  });
+			// Exibir proxima questao
+			if (currentQuestion < listOfQuestions.length){
+				modelHTML(listOfQuestions[currentQuestion])
+			}
+			else { 
+				//Finalizou Quiz				
+				localStorage.setItem("answer", answer)
+				location.href = 'result.html'
+			}
+			
+			// Atualiza barra de progresso
+			uploadProgressBar += (100 / listOfQuestions.length)
+			progress_bar.style.width = uploadProgressBar+"%"
+		})
+  })
 
 
 //////////////// Help Functions //////////////////////////////
 
 	// Fucao que modela as questoes no HTML
 	function modelHTML(questions) {
-		var title = questions.title;
-		var alternatives = questions.alternatives;
-		if ($('.radio').length !== 0)
-			$('.radio').remove();
+		var title = questions.title
+		var alternatives = questions.alternatives
+		var radioElement = document.getElementsByClassName('radio')
+		
+		if (radioElement.length !== 0)
+			removeElement(radioElement)
 
-		$('.panel-title').html(title);
-		for (var i = 0; i < alternatives.length; i++) {
-			var value = i+1;
-		  	$('.panel-body').append("<div class=\"radio\">\n"+
-		  	       "	<label>\n"+
-		  	       "		<input type=\"radio\" name=\"value\" id=\"optionsRadios"+i+1+"\" value=\""+value.toString()+"\"> "+alternatives[i]+"\n"+
-		  	       "	</label>\n"+
-		  	       "</div>\n");
-		}
+		document.getElementById('title').innerHTML = title
+		var value = 0
+		var form = document.forms[0]
+		addActionInListOfElements(alternatives, alternative => {
+			value += 1
+			var content = "<div class=\"radio\">\n"+
+						"	<label>\n"+
+						"		<input type=\"radio\" name=\"question\" id=\"optionsRadios"+value+"\" value=\""+value.toString()+"\"> "+alternative+"\n"+
+						"	</label>\n"+
+						"</div>\n";
+			appendElement(form, 'div', content).classList.add('radio')
+		})
+
 		/*Activate button when replying*/
-		$('input:radio[name=value]').click(function () {
-			$('#confirm').removeClass('disabled');
-		});
+		var inputs = document.getElementsByTagName('input')
+		addActionInListOfElements(inputs, element => {
+			element.addEventListener('click', () => {
+				document.getElementById('confirm').classList.remove('disabled')
+			})
+		})
+	}
+	
+	function removeElement(element) {
+		for(var i = element.length - 1; i >= 0; i--)
+			if(element[i] && element[i].parentElement)
+				element[i].parentElement.removeChild(element[i])
 	}
 
-});
+	// executa uma acao para cada elemento da lista
+	function addActionInListOfElements (list, action) {
+		for (var i = list.length - 1; i >= 0; i--) {
+			if (list[i]) {
+				action(list[i])
+			}
+		}
+	}
+
+	// append element child 
+	function appendElement(parent, elementType, content) {
+		var div = document.createElement(elementType)
+		div.innerHTML = content
+		parent.appendChild(div)
+		return div
+	}
+
+})()
--- public/js/review.js
+++ public/js/review.js
@@ -1,21 +1,57 @@
-$(function () {
+//(() => {})()
+(() => {
+
 	/*Limpar dados salvos no browser*/
-	localStorage.clear();
-	$('#btn-quiz').click(function () {
+	localStorage.clear()
+
+	checkQuiz = () => {
 		/*Avalia se o usuario ja fez o quiz*/
 		if (localStorage.getItem("result"))
-			location.href = 'result.html';
+			location.href = 'result.html'
 		else
-			location.href = 'quiz-op1.html';
-	});
+			location.href = 'quiz.html'
+	}
 
-	var btnReview = $("#review").children("button");
 
-	$(".review-box").each(function (i) {
-		var currentDiv = this;
-		$(btnReview[i]).click(function () {
-				$(currentDiv).toggle();
-		});
-	});
+	var rev = document.getElementById("review")
+	var listBtnReview = getChildrensByTag(rev, 'button')
+	var reviewBox = document.getElementsByClassName('review-box')
+	var index = 0
+	addActionInListOfElements(reviewBox, box => {
+		listBtnReview[index].addEventListener('click', () => {
+			toggleElement(box)
+		})
+		index++	
+	})
 
-});
+	
+	
+	/** Helper **/
+	function getChildrensByTag(element, tagName) {
+		var childrens = element.children
+		var childrensByTag = []
+		for (var i = 0; i < childrens.length; i++) {
+			var element = childrens[i];
+			if (element) {
+				if (element.tagName === tagName.toUpperCase()) {
+					childrensByTag.push(element)
+				}
+			}
+			
+		}
+		return childrensByTag
+	}
+
+	function addActionInListOfElements (list, action) {
+		for (var i = 0; i < list.length; i++) {
+			if (list[i]) {
+				action(list[i])
+			}
+		}
+	}
+
+	function toggleElement(element) {
+		element.style.display = (element.style.display == 'none') ? 'block' : 'none';
+	}
+
+})()
