d263281955bace5159bcb8f720768ef8b98322d0
Matheus Lima <matheus.lima@lavid.ufpb.br>
2019-04-08 16:28:33 -0300

    fix: port

--- web-server.js
+++ web-server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const server = express()
-const port = 8080
+const express = require('express');
+const path = require('path');
+const server = express();
+const PORT = process.env.PORT || 5000;
 
-server.use(express.static(path.join(__dirname, './public')))
-server.listen(port, () => console.log(`The server is running on http://localhost:${port}`))
+server.use(express.static(path.join(__dirname, './public')));
+server.listen(PORT, () => console.log(`The server is running on http://localhost:${PORT}`));
